7f6d318561b1f9c355f3f6b700290c6947f61e65
Ravindra <ravindra.kk@doodleblue.com>
2021-08-20 09:36:24 +0530

    added the folder structure for apis

---
+++ src/config/Api.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'http://localhost:4500/api/v1';
+
+export const endPoints = {
+  user: {
+    GET_LIST: `${BASE_URL}/users`,
+    CREATE: `${BASE_URL}/user`,
+    UPDATE: `${BASE_URL}/user`,
+    REMOVE: `${BASE_URL}/user`
+  }
+};
