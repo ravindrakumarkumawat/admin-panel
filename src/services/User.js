1b2ad23f9eabb610e47e06e4299b7387b1adeb5e
Ravindra <ravindra.kk@doodleblue.com>
2021-08-20 10:04:28 +0530

    added the user service

---
+++ src/services/User.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { endPoints } from '../config/Api';
+
+export const getUser = async (page) => axios.get(endPoints.user.GET_LIST);
+export const createUser = async (data) => axios.post(endPoints.user.CREATE, data);
+export const updateUser = async (id, data) => axios.patch(`${endPoints.user.UPDATE}/${id}`, data);
+export const removeUser = async (id) => axios.delete(`${endPoints.user.UPDATE}/${id}`);
