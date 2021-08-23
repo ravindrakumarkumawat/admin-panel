55f8c31ef25be02fa8efe5fcef1672412a06e7b2
Ravindra <ravindra.kk@doodleblue.com>
2021-08-23 09:09:47 +0530

    uninstalled the bootstrap

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { HelmetProvider } from 'react-helmet-async';
 
 // Antd and bootstrap
 import 'antd/dist/antd.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 //
 import App from './App';
